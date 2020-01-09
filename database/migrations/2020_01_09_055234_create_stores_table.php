482bcc24c92f672eb78115546bfadd3516db7999
wisnu putra dewa <com.owl.minerva@gmail.com>
2020-01-09 13:44:55 +0700

    push 09/01/2020 01.44am

---
+++ database/migrations/2020_01_09_055234_create_stores_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateStoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stores', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('full_name');
+            $table->string('full_address');
+            $table->string('number_ktp');
+            $table->string('ktp');
+            $table->string('ktp_and_user');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stores');
+    }
+}
